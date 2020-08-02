50252decf042f16b843175de5702dc06c419819b
Aaron <troublor@live.com>
2020-08-02 22:09:57 +0800

    add ethash, executables docker

---
+++ scripts/docker/executables/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# docker context should be darcher-go-ethereum root
+
+DARCHER_GO_ETHEREUM_PATH=../../../
+# build darcher-go-ethereum executables
+IMAGE_NAME=darcher/go-ethereum/executables
+docker build --progress=plain --no-cache=true -f Dockerfile -t ${IMAGE_NAME} "${DARCHER_GO_ETHEREUM_PATH}"
