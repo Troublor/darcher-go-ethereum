3ee15add6d1018bc82e114cfff33aac49a48f762
Aaron <troublor@live.com>
2020-11-30 13:50:19 +0800

    make shell work in both ubuntu and macos

--- scripts/docker/ethash/build.sh
+++ scripts/docker/ethash/build.sh
@@ -1,13 +1,21 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
 # build darcher/ethash docker image
 
 IMAGE_NAME=darcher/ethash
 
-if [[ ! -d ".ethash" ]]; then
-  # TODO default ethash path for other platforms
-  # MacOS default ethash path
-  cp -r ~/Library/Ethash ./.ethash
+DIR=$( dirname "${BASH_SOURCE[0]}" )
+
+if [ "$(uname -s)" == "Linux" ]; then
+  if [[ ! -d "$DIR/.ethash" ]]; then
+    # Linux default ethash path
+    cp -r ~/.ethash "$DIR"/.ethash
+  fi
+elif [ "$(uname -s)" == "Darwin" ]; then
+  if [[ ! -d "$DIR/.ethash" ]]; then
+    # MacOS default ethash path
+    cp -r ~/Library/Ethash "$DIR"/.ethash
+  fi
 fi
 
-docker build --no-cache -t $IMAGE_NAME -f ./Dockerfile ./
+docker build --no-cache -t $IMAGE_NAME -f "$DIR"/Dockerfile "$DIR"
--- scripts/docker/executables/build.sh
+++ scripts/docker/executables/build.sh
@@ -1,7 +1,9 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 # docker context should be darcher-go-ethereum root
 
-DARCHER_GO_ETHEREUM_PATH=../../../
+DIR=$( dirname "${BASH_SOURCE[0]}" )
+
+DARCHER_GO_ETHEREUM_PATH=$DIR/../../../
 # build darcher-go-ethereum executables
 IMAGE_NAME=darcher/go-ethereum/executables
-docker build --progress=plain --no-cache=true -f Dockerfile -t ${IMAGE_NAME} "${DARCHER_GO_ETHEREUM_PATH}"
+docker build --progress=plain --no-cache=true -f "$DIR"/Dockerfile -t ${IMAGE_NAME} "${DARCHER_GO_ETHEREUM_PATH}"
