5d5176e17a53b4ad8ba098f480722539aec58ac9
Wuqi Aaron ZHANG <troublor9816@gmail.com>
2021-03-08 13:42:20 +0800

    :bookmark: start "next" development phase The "latest" docker tag is reserved for FSE21 submission review

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -3,5 +3,5 @@
 DIR=$( dirname "${BASH_SOURCE[0]}" )
 # shellcheck source=./entry-env.sh
 source "$DIR"/entry-env.sh
-IMAGE_NAME=darcherframework/go-ethereum:latest
+IMAGE_NAME=darcherframework/go-ethereum:next
 docker build --progress=plain --build-arg BLOCKCHAIN_DEFAULT_DIR=$BLOCKCHAIN_DEFAULT_DIR -t $IMAGE_NAME "$DIR"
